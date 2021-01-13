21b5e3259934675bf2b30c421537998de669e63a
ahmer7aa <ahmer5253@gmail.com>
2021-01-13 07:09:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# progress_indicator
+
+A new Flutter application.
+
+## Getting Started
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+Some Interpretations
+
+A Flutter Progress Indicator: ```Linear-Circular``` and ```Determinate-Indeterminate```
+
+# Screenshots
+
+# Working
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.file_download),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
